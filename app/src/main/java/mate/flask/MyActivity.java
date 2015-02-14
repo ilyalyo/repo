922ae8766898d7b48ec7596db4a9161e52edc2ac
ilya_g <ilya4041@yandex.ru>
2015-02-14 21:23:09 +0300

    Add Statistic Activiti

--- app/src/main/java/mate/flask/MyActivity.java
+++ app/src/main/java/mate/flask/MyActivity.java
@@ -1,54 +1,38 @@
 package mate.flask;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
-import android.media.Image;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.AbsoluteLayout;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.util.ArrayList;
 
 
 public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "mate.flask.MyActivity.user_id";
+    private int USER_ID=25719572;
+
+    public static dbInteraction db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        VK vk=new VK();
-        User user=vk.getUser(25719572);
-        user.print();
-        final dbInteraction db=new dbInteraction(25719572);
+        db=new dbInteraction(USER_ID);
         db.initialize();
 
         ArrayList<User> users=db.getMyVictims();
         db.getDataInterval(10379683);
-
 
         //final LinearLayout lm = (LinearLayout) findViewById(R.id.linear);
         ScrollView lm = (ScrollView) findViewById(R.id.scrollView);
@@ -67,6 +51,7 @@ public class MyActivity extends Activity {
 
                 WebView web = new WebView(this);
                 web.loadUrl(users.get(i).photo);
+
                 web.setBackgroundColor(Color.argb(1, 0, 0, 0));
                 btn.setOnClickListener(new View.OnClickListener() {
                     int counter = 0;
@@ -82,16 +67,21 @@ public class MyActivity extends Activity {
                     }
 
                     public void onClick(View v) {
-                        TextView textv = new TextView(v.getContext());
+                        try{
+                            openStatistic(v,btn.getId());
+                        }
+                        catch(Exception e){
+                            Log.e("log_tag", "Error in view " + e.toString());
+                        }
+                      /*  TextView textv = new TextView(v.getContext());
                         if (counter == 0)
                             init();
                         textv.setText(arr.get(counter).date.toString() + " | " + arr.get(counter).isOnline);
                         if(arr.get(counter++).isOnline)
                             textv.setBackgroundColor(Color.argb(150, 255, 92, 0));
-                        ll.addView(textv);
+                        ll.addView(textv);*/
                     }
                 });
-
                 ll.addView(web);
                 ll.addView(btn);
                 lmm.addView(ll);
@@ -101,24 +91,14 @@ public class MyActivity extends Activity {
         }catch(Exception e) {
             Log.e("log_tag", "Error in http connection " + e.toString());
         }
-        /*
-        HTTPGET h=new HTTPGET();
-        TextView textv = (TextView)findViewById(R.id.textView);
-        try{
-            textv.setText(  h.execute("12").get());
-        }catch(Exception e){
-            Log.e("log_tag", "Error in http connection "+e.toString());
-        }
-        */
     }
-    /*
-    public void changText() {
-        TextView textv = (TextView)findViewById(R.id.textView);
-        textv.setText("Text Has Been Changed");
-        BufferedReader in = null;
-        String data = null;
+
+    public void openStatistic(View view,int id) {
+        Intent intent = new Intent(this, Statistic.class);
+        intent.putExtra(EXTRA_MESSAGE, id);
+        startActivity(intent);
     }
-    */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/mate/flask/Statistic.java
@@ -0,0 +1,150 @@
+package mate.flask;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+
+public class Statistic extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(new DrawGraph(this));
+
+
+        DrawGraph drawGraph = new DrawGraph(this);
+        drawGraph.setBackgroundColor(Color.WHITE);
+        ScrollView scrollView = new ScrollView(this);
+
+        scrollView.addView(drawGraph);
+        setContentView(scrollView);
+
+        //Intent intent = getIntent();
+
+       // int user_id= intent.getIntExtra(MyActivity.EXTRA_MESSAGE,-1);
+
+     //   ArrayList<DataRecord> dateRecords=MyActivity.db.getDataInterval(user_id);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.statistic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawGraph extends View {
+
+        Paint p;
+        Bitmap bitmap;
+        int blockHeight=50;
+        int height;
+        ArrayList<DataRecord> dateRecords;
+
+        public DrawGraph(Context context) {
+            super(context);
+
+            Intent intent = getIntent();
+
+            int user_id= intent.getIntExtra(MyActivity.EXTRA_MESSAGE,-1);
+
+            dateRecords=MyActivity.db.getDataInterval(user_id);
+
+            bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.RGB_565);
+
+            p = new Paint();
+
+            height =dateRecords.size()*blockHeight+100;
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            // Compute the height required to render the view
+            // Assume Width will always be MATCH_PARENT.
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height =dateRecords.size()*blockHeight+100; // Since 3000 is bottom of last Rect to be drawn added and 50 for padding.
+            setMeasuredDimension(width, height);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // TODO Auto-generated method stub
+            canvas.drawColor(Color.WHITE);
+
+            height=0;
+            int rect_x=10;
+            int line_x=200;
+            boolean lastStatus=! dateRecords.get(0).isOnline;
+            int inSequence=0;
+
+            for(int i =0;i<dateRecords.size();i++){
+                DataRecord record=dateRecords.get(i);
+
+                if(lastStatus!=record.isOnline) {
+                    inSequence=0;
+
+                    p.setTextSize(30);
+                    String shortDate = new SimpleDateFormat("HH:mm").format(record.date);
+
+                    p.setColor(Color.rgb(0, 0, 0));
+                    canvas.drawText(shortDate, line_x + 20, height - 10, p);
+                    p.setColor(Color.rgb(2,119,189));
+                    canvas.drawLines(new float[]{rect_x, height , line_x + 100, height }, p);
+                }
+                else{
+                    inSequence++;
+                }
+                if(inSequence<5){
+                    Rect rect = new Rect(rect_x,height,rect_x+100,height+blockHeight);
+                    if(record.isOnline)
+                        p.setColor(Color.rgb(2,119,189));
+                    else
+                        p.setColor(Color.rgb(255, 255, 255));
+
+                    p.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(rect, p);
+
+                    height+=blockHeight;
+                }
+
+                lastStatus=record.isOnline;
+            }
+            p.setColor(Color.rgb(0, 0, 0));
+            canvas.drawText("END", line_x + 20, height - 10, p);
+            height+=100;
+          //  this.requestLayout();
+        }
+
+    }
+}
